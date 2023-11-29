53ef9e4271ce89a3319c39f929fb4a5143dfda36
Safa Fidan <75932545+sfidan@users.noreply.github.com>
2023-11-29 22:24:35 +0300

    Add files via upload

---
+++ Mailtips.ps1
@@ -0,0 +1,19 @@
+﻿# Exchange PowerShell modülünü yükle
+ 
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+ 
+#Kullanıcıları ve Mailtips bilgileri tanımla
+ 
+$users = @("testuser1@contoso.com","testuser2@contoso.com","testuser3@contoso.com")
+$mailtips = @("Lutfen, Sifrenizi kimseyle paylasmayın"," Supheli emailler için addin butonunu kullanınız")
+ 
+# Her bir kullanıcı için rastgele bir cümle seçme ve MailTips olarak atama
+ 
+foreach ($user in $users) 
+{
+    $mailbox = get-mailbox $user
+    # Rastgele bir cümle seçme
+    $randomSentence = $mailtips | Get-Random
+    # Kullanıcıya rastgele cümleyi MailTips olarak atama
+    Set-Mailbox -Identity $mailbox -MailTip $randomSentence
+}
